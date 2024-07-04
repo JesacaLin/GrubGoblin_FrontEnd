56f1a842ee85f8b113843a367b7c4d0d378b1dde
Qiao Juan Lin <JesacaLin@gmail.com>
2024-07-04 13:57:19 -0400

    hard coding html elements work with api

--- js/script.js
+++ js/script.js
@@ -3,9 +3,29 @@
 //initialize fetch api
 //use forEach to set the data
 
-const allCards = document.querySelectorAll("article");
 
-const locateMeButton = document.getElementById("locate-me-button");
-locateMeButton.addEventListener("click", () => {
-  console.log("button clicked!");
-});
+
+async function getDealData() {
+  const response = await fetch('http://localhost:8080/deal/details');
+  const data = await response.json();
+  console.log('inside getDealData')
+  return data
+}
+
+
+async function populateDealCards() {
+  const dealData = await getDealData();
+  const dealCards = document.querySelectorAll("article");
+  console.log(dealData)
+  
+  dealData.forEach((deal, index) => {
+    const dealCard = dealCards[index];
+    dealCard.querySelector(".deal-card-name").textContent = deal.placeName;
+    dealCard.querySelector(".deal-card-type").textContent = deal.typeOfDeal;
+    console.log('inside forEach')
+  })
+  console.log('inside populateDealCards')
+}
+
+document.getElementById("locate-me-button").addEventListener('click', populateDealCards);
+
