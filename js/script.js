09a367f3a316bf52aff29306ee74fd14c34b9f62
Qiao Juan Lin <JesacaLin@gmail.com>
2024-07-05 17:27:03 -0400

    search bar works

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-async function getDealData() {
-  const response = await fetch('http://localhost:8080/deal/details');
+async function getDealData(endpoint) {
+  const response = await fetch(endpoint);
   const data = await response.json();
-  console.log('inside getDealData')
+  // console.log('inside getDealData')
   return data
 }
 
@@ -32,18 +32,46 @@ const dealCardTemplate = (deal) => `
   </article>
 `;
 
-async function createAndAppendDealCards() {
-  const dealData = await getDealData();
+async function createAndAppendDealCards(endpoint) {
+  const dealData = await getDealData(endpoint);
   const dealContainer = document.getElementById('deal-cards');
 
   dealData.forEach(deal => {
     dealContainer.insertAdjacentHTML('beforeend', dealCardTemplate(deal));
-    console.log('inside forEach')
+    
   });
-
-  console.log('inside createAndAppendDealCards')
 }
 
+//get all deal with full detail
+document.getElementById('locate-me-button').addEventListener('click', ()=> {
+  const endpoint = 'http://localhost:8080/deal/details';
+  createAndAppendDealCards(endpoint);
+});
 
-document.getElementById("locate-me-button").addEventListener('click', createAndAppendDealCards);
+//get deals by keyword or days of the week
+  document.getElementById('search-button').addEventListener('click', () => {
+    let userInput = document.getElementById('search-input').value.toLowerCase();
+    console.log(`userInput: ${userInput}`)
+    const daysOfWeekArr = [
+      {monday: 'm'}, 
+      {tuesday: 't'}, 
+      {wednesday: 'w'}, 
+      {thursday: 'h'}, 
+      {friday: 'f'}, 
+      {saturday: 's'}, 
+      {sunday: 'u'}
+    ]
+    //finds and return first encounter of element or undefine
+    let findDay = daysOfWeekArr.find(dayObj => dayObj[userInput]);
+      if (findDay) {
+        let value = findDay[userInput]
+        const endpoint = `http://localhost:8080/deal/details/day/${value}`;
+        createAndAppendDealCards(endpoint);
+      } else {
+        const endpoint = `http://localhost:8080/deal/details/keyword/${userInput}`;
+        createAndAppendDealCards(endpoint);
+      }
 
+    document.getElementById('search-input').value = '';
+
+  })
